6dead0a906cb0324c5912a2bd01d2893bb3043f8
Eras Notra <xpower3d@yahoo.com>
2014-06-12 20:49:36 -0700

    restructuring setup

--- generics/__init__.py
+++ generics/__init__.py
@@ -1,9 +1,7 @@
 from .analyze_html import Analyze_HTML
 from .fields import RestrictedFileField
-from .functions import (int_with_default, url_to_edit_object, url_to_list_view_of_object,
-                        get_or_cache, serial_func, serial_block_check, serial_block_begin,
-                        serial_block_end, model_fields_list, model_field_type,
-                        )
-from .models import GenericManager
-from .tasks import celery_progressbar_stat
-from .views import task_api
+from .functions import *
+from .models import *
+from .tasks import *
+from .views import *
+from .management.commands import *
